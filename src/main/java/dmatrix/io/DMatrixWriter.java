b05e4b599240271f6c361401161b8124bd927747
Jordan Zhang <jordan.of.zhang@gmail.com>
2016-05-12 16:54:32 +0100

    Subclass the sparse dmatrix writer

---
+++ src/main/java/dmatrix/io/DMatrixWriter.java
@@ -0,0 +1,49 @@
+package dmatrix.io;
+
+import java.io.*;
+import java.nio.ByteBuffer;
+import java.nio.file.Paths;
+
+/**
+ * Base serializer for density matrices.
+ * <p>
+ * Created by zhuoranzhang on 5/12/16.
+ */
+public abstract class DMatrixWriter {
+    BufferedOutputStream outputStream;
+    String word;
+
+    public DMatrixWriter(String word, String matricesPath) {
+        this.word = word;
+        try {
+            File outputFile = new File(Paths.get(matricesPath, word + ".bin").toString());
+            if (!outputFile.exists()) {
+                if (!outputFile.getParentFile().exists()) {
+                    outputFile.getParentFile().mkdirs();
+                }
+                outputFile.createNewFile();
+            }
+            outputStream = new BufferedOutputStream(new FileOutputStream(outputFile, false));
+        } catch (FileNotFoundException e) {
+            System.out.println(String.format("Unable to open file for word: %s.", word));
+        } catch (IOException e) {
+            System.out.println(String.format("Unable to create file for word: %s.", word));
+        }
+    }
+
+    public void close() {
+        try {
+            outputStream.close();
+        } catch (IOException e) {
+            System.out.println(String.format("IOException thrown for word: %s. Matrix potentially corrupted.", word));
+        }
+    }
+
+    static byte[] int2ByteArray(int value) {
+        return ByteBuffer.allocate(4).putInt(value).array();
+    }
+
+    static byte[] float2ByteArray(float value) {
+        return ByteBuffer.allocate(4).putFloat(value).array();
+    }
+}
--- src/main/java/dmatrix/io/SparseDMatrixWriter.java
+++ src/main/java/dmatrix/io/SparseDMatrixWriter.java
@@ -1,34 +1,18 @@
 package dmatrix.io;
 
-import java.io.*;
-import java.nio.ByteBuffer;
-import java.nio.file.Paths;
+import dmatrix.SparseDMatrix;
+
+import java.io.IOException;
 
 /**
  * Serializer for sparse density matrices.
- * <p>
+ *
  * Created by zhuoranzhang on 5/12/16.
  */
-public class SparseDMatrixWriter {
-    private BufferedOutputStream outputStream;
-    private String word;
+public class SparseDMatrixWriter extends DMatrixWriter {
 
     public SparseDMatrixWriter(String word, String matricesPath) {
-        this.word = word;
-        try {
-            File outputFile = new File(Paths.get(matricesPath, word + ".bin").toString());
-            if (!outputFile.exists()) {
-                if (!outputFile.getParentFile().exists()) {
-                    outputFile.getParentFile().mkdirs();
-                }
-                outputFile.createNewFile();
-            }
-            outputStream = new BufferedOutputStream(new FileOutputStream(outputFile, false));
-        } catch (FileNotFoundException e) {
-            System.out.println(String.format("Unable to open file for word: %s.", word));
-        } catch (IOException e) {
-            System.out.println(String.format("Unable to create file for word: %s.", word));
-        }
+        super(word, matricesPath);
     }
 
     public void writeEntry(int x, int y, float value) {
@@ -41,19 +25,4 @@ public class SparseDMatrixWriter {
         }
     }
 
-    public void close() {
-        try {
-            outputStream.close();
-        } catch (IOException e) {
-            System.out.println(String.format("IOException thrown for word: %s. Matrix potentially corrupted.", word));
-        }
-    }
-
-    private static byte[] int2ByteArray(int value) {
-        return ByteBuffer.allocate(4).putInt(value).array();
-    }
-
-    private static byte[] float2ByteArray(float value) {
-        return ByteBuffer.allocate(4).putFloat(value).array();
-    }
 }
