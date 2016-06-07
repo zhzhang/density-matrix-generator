65ae0a908d3045a33768291f0c35a7e03694475e
Jordan Zhang <jordan.of.zhang@gmail.com>
2016-06-07 21:41:39 +0100

    Catch IOException instead of file not found exception in dmatrixwriter

--- src/main/java/dmatrix/io/DMatrixWriter.java
+++ src/main/java/dmatrix/io/DMatrixWriter.java
@@ -24,8 +24,6 @@ public abstract class DMatrixWriter {
                 outputFile.createNewFile();
             }
             outputStream = new BufferedOutputStream(new FileOutputStream(outputFile, false));
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
